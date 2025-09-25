2e0e5bd4aafc2b96a9d71639d62a7a6c9f9675b2
Egor0108 <ehor01012008@gmail.com>
2025-09-25 21:34:29 +0300

    Вариант 9

--- Task_6.cpp
+++ Task_6.cpp
@@ -2,14 +2,13 @@
 #include <cmath>
 using namespace std;
 int main (){
- double A, B, x, d, C, K, Y;
- cout << "Введите чему равны x, d, C, K: ";
- cin >>x>>d>>C>>K;
- A=log10(x);
- B=x+exp(d);
- Y=(A+B)-(C*C)/K;
- cout << "Ответ равен: "<<Y;
-
- return 0;
+ double A, B, C, D, K, Y, x, z, p;
+ cout <<"Введите чему равны x, z, p, K, C, D: ";
+ cin >>x>>z>>p>>K>>C>>D;
+ B=abs(p-x);
+ A=sin(x)-z;
+ Y=(A+B)*(A*B)-K/(C*D);
+ cout << "Ответ равен : "<<Y;
+return 0;
 
 }
