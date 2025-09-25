e321025e76c5e5ba7db9bf9ef041b64b6e86bb46
Egor0108 <ehor01012008@gmail.com>
2025-09-25 21:52:37 +0300

    Вариант 6 исправленный

--- Task_6.cpp
+++ Task_6.cpp
@@ -2,12 +2,11 @@
 #include <cmath>
 using namespace std;
 int main (){
- double A, D, C, K, x, y, S, z;
- cout << "Ведите чему равны x, y,C,K";
+ double A, D, C, K, x, y, S;
+ cout << "Ведите чему равны x, y,C,K: ";
  cin >>x>>y>>C>>K;
  A=x+y;
- z=C-A;
- D=abs(z);
+ D=abs(C-A);
  S=10.1+A/C+D/(K*K);
  cout << "Ответ равен : "<<S;
   
